2b40d270fb7e462a90cdf65f514574c53ea60afc
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-18 15:32:37 +0700

    added getSlugHash()

---
+++ src/getSlugHash.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Get slug hash from path
+ * @param {string} path
+ * @return {string}
+ */
+const getSlugHash = (path) => {
+  const regex = /^\/(?<author>\w{0,127})\/(?<pen>\w{3})\/(?<slug>\w{1,127}).{0,127}$/;
+  const matches = regex.exec(path);
+
+  return matches.groups.slug;
+};
+
+module.exports = getSlugHash;
