40be040d8f8ccc166fefe4a370b3512db4017da9
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-18 15:33:04 +0700

    refactored

--- src/parseParams.js
+++ src/parseParams.js
@@ -1,26 +1,43 @@
 'use strict';
 
 /**
+ * Parse param string
+ * @param {string} str
+ * @return {object}
+ */
+const parseString = (str) => {
+  const options = {};
+
+  str.split(';').forEach((pair) => {
+    const [key, value] = pair.split(':');
+    options[key.trim()] = value.trim();
+  });
+
+  return options;
+};
+
+/**
+ * is JSON?
+ * @param {string} str
+ * @return {boolean}
+ */
+const isJSON = (str) => {
+  const cleanStr = str.trim();
+  return (cleanStr[0] === '{') && (cleanStr[cleanStr.length -1] === '}');
+};
+
+/**
  * Parse params string
  * @param {string} params parameters string
  * @return {object} parsed parameters object
  */
-const parseParams = (params = '') => {
-  let cleanParams = params !== null ? params : '';
+const parseParams = (params) => {
+  let cleanParams = params ? params : {};
 
   if (typeof cleanParams === `string` ) {
-    if ((cleanParams[0] === '{') && (cleanParams[cleanParams.length - 1] === '}')) {
-      cleanParams = JSON.parse(cleanParams);
-    } else {
-      const tmpOpts = {};
-      if (cleanParams.length > 0) {
-        cleanParams.split(';').forEach((option) => {
-          const [key, value] = option.split(':');
-          tmpOpts[key] = value;
-        });
-      }
-      cleanParams = tmpOpts;
-    }
+    cleanParams = isJSON(cleanParams) ?
+      JSON.parse(cleanParams) :
+      parseString(cleanParams);
   }
   return cleanParams;
 };
