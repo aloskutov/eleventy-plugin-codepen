7969f568e450d46d62441bbf9c29b9f684f44db6
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-03-06 16:27:44 +0700

    added test parseParams

---
+++ test/parseParams.test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const parseParams = require('../src/parseParams');
+
+describe('Test #1. Parse empty params', () => {
+  test.each([
+    {result: {}},
+    {param: '', result: {}},
+    {param: null, result: {}},
+  ])('parseParams($param)', ({param, result}) => {
+    expect(parseParams(param)).toStrictEqual(result);
+  });
+});
+
+describe('Test #2. Parse object params', () => {
+  test.each([
+    {param: {'height': 650, 'class': 'codepen-resizable', 'theme': 'light', 'tab': 'js,result'}, result: {'height': 650, 'class': 'codepen-resizable', 'theme': 'light', 'tab': 'js,result'}},
+    {param: {'height': 450, 'theme': 'light', 'tab': 'js,result'}, result: {'height': 450, 'theme': 'light', 'tab': 'js,result'}},
+    {param: {'height': 850, 'class': 'codepen-resizable', 'tab': 'js,result'}, result: {'height': 850, 'class': 'codepen-resizable', 'tab': 'js,result'}},
+  ])('parseParams($param)', ({param, result}) => {
+    expect(parseParams(param)).toStrictEqual(result);
+  });
+});
+
+describe('Test #3. Parse JSON string', () => {
+  test.each([
+    {param: {'height': '650', 'class': 'codepen-resizable', 'theme': 'light', 'tab': 'js,result'}, result: {'height': '650', 'class': 'codepen-resizable', 'theme': 'light', 'tab': 'js,result'}},
+    {param: {'height': '450', 'theme': 'light', 'tab': 'js,result'}, result: {'height': '450', 'theme': 'light', 'tab': 'js,result'}},
+    {param: {'height': '850', 'class': 'codepen-resizable', 'tab': 'js,result'}, result: {'height': '850', 'class': 'codepen-resizable', 'tab': 'js,result'}},
+  ])('parseParams($param)', ({param, result}) => {
+    expect(parseParams(param)).toStrictEqual(result);
+  });
+});
+
+describe('Test #4. Parse string', () => {
+  test.each([
+    {param: 'height:650;class:codepen-resizable;theme:light;tab:js,result', result: {'height': '650', 'class': 'codepen-resizable', 'theme': 'light', 'tab': 'js,result'}},
+    {param: 'height:450;theme:light;tab:js,result', result: {'height': '450', 'theme': 'light', 'tab': 'js,result'}},
+    {param: 'height:850;class:codepen-resizable;tab:js,result', result: {'height': '850', 'class': 'codepen-resizable', 'tab': 'js,result'}},
+  ])('parseParams($param)', ({param, result}) => {
+    expect(parseParams(param)).toStrictEqual(result);
+  });
+});
+
+describe('Test #5. Throw SyntaxError', () => {
+  test('', () =>{
+    const param = '{height: 650, class: codepen-resizable, theme: light, tab: js,result}';
+    expect(() => parseParams(param)).toThrow(SyntaxError);
+  });
+});
