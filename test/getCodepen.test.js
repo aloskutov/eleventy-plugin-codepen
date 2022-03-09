746c98dda26cbbcc478de5407e3e001b8cfe42ac
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-03-09 19:26:06 +0700

    updated test

--- test/getCodepen.test.js
+++ test/getCodepen.test.js
@@ -35,4 +35,30 @@ describe('Get codepen html', () => {
   <span><a href="${url}">See the Pen </a></span></p>`;
     expect(getCodepenHtml(options, slugHash)).toBe(result);
   });
+
+  test('Test #3 with JS', () => {
+    const slugHash = 'jOmaBQK';
+    options.insertJS = true;
+    const url = 'https://codepen.io//pen/jOmaBQK';
+    const result = `<p class="codepen"
+  data-class="${options.class}"
+  data-height="${options.height}"
+  data-theme-id="${options.theme}"
+  data-default-tab="${options.tab}"
+  data-slug-hash="jOmaBQK">
+  <span><a href="${url}">See the Pen </a></span></p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>`;
+    expect(getCodepenHtml(options, slugHash)).toBe(result);
+  });
+
+  test('Test #4 Throw Error empty options', () =>{
+    let option;
+    const url = 'https://codepen.io/gayane-gasparyan/pen/jOmaBQK';
+    expect(() => getCodepenHtml(option, url)).toThrow(TypeError);
+  });
+
+  test('Test #5 Throw Error empty url', () =>{
+    let url;
+    expect(() => getCodepenHtml(options, url)).toThrow(TypeError);
+  });
 });
