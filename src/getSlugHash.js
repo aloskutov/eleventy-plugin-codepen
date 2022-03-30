b3a60802c0cb7f2de973b8afdb924b693694d91b
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-03-30 15:42:20 +0700

    added support for long url /author/pen/slug/hash

--- src/getSlugHash.js
+++ src/getSlugHash.js
@@ -6,10 +6,10 @@
  * @return {string}
  */
 const getSlugHash = (path) => {
-  const regex = /^\/(?<author>[\w-]{0,127})\/(?<pen>\w{3})\/(?<slug>\w{1,127}).{0,127}$/u;
+  const regex = /^\/(?<author>[\w-]{0,127})\/(?<pen>\w{3})\/(?<slug>\w{1,127})(?:\/(?<hash>\w{1,127})){0,1}.{0,127}$/u;
   const matches = regex.exec(path);
 
-  return matches.groups.slug;
+  return matches.groups.hash ? matches.groups.hash : matches.groups.slug ;
 };
 
 module.exports = getSlugHash;
--- test/getCodepen.test.js
+++ test/getCodepen.test.js
@@ -11,19 +11,20 @@ const options = {
 };
 
 describe('Get codepen html', () => {
-  test('Test #1 with URL', () => {
+  test('Test #1 URL', () => {
+    const slugHash = 'jOmaBQK';
     const url = 'https://codepen.io/gayane-gasparyan/pen/jOmaBQK';
     const result = `<p class="codepen"
   data-class="${options.class}"
   data-height="${options.height}"
   data-theme-id="${options.theme}"
   data-default-tab="${options.tab}"
-  data-slug-hash="jOmaBQK">
+  data-slug-hash="${slugHash}">
   <span><a href="${url}">See the Pen </a></span></p>`;
     expect(getCodepenHtml(options, url)).toBe(result);
   });
 
-  test('Test #2 with SlugHash', () => {
+  test('Test #2 SlugHash', () => {
     const slugHash = 'jOmaBQK';
     const url = 'https://codepen.io//pen/jOmaBQK';
     const result = `<p class="codepen"
@@ -31,12 +32,12 @@ describe('Get codepen html', () => {
   data-height="${options.height}"
   data-theme-id="${options.theme}"
   data-default-tab="${options.tab}"
-  data-slug-hash="jOmaBQK">
+  data-slug-hash="${slugHash}">
   <span><a href="${url}">See the Pen </a></span></p>`;
     expect(getCodepenHtml(options, slugHash)).toBe(result);
   });
 
-  test('Test #3 with JS', () => {
+  test('Test #3 SlugHash with JS', () => {
     const slugHash = 'jOmaBQK';
     options.insertJS = true;
     const url = 'https://codepen.io//pen/jOmaBQK';
@@ -45,7 +46,7 @@ describe('Get codepen html', () => {
   data-height="${options.height}"
   data-theme-id="${options.theme}"
   data-default-tab="${options.tab}"
-  data-slug-hash="jOmaBQK">
+  data-slug-hash="${slugHash}">
   <span><a href="${url}">See the Pen </a></span></p>
 <script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>`;
     expect(getCodepenHtml(options, slugHash)).toBe(result);
@@ -59,15 +60,16 @@ describe('Get codepen html', () => {
     expect(() => getCodepenHtml(options)).toThrow(TypeError);
   });
 
-  test('Test #6 with URL', () => {
+  test('Test #6 URL with long slug hash', () => {
     const url = 'https://codepen.io/sdras/pen/PowKVyN/faca988359f13c8a98d2a6fe81ef43fa';
+    const slugHash = 'faca988359f13c8a98d2a6fe81ef43fa';
     options.insertJS = false;
     const result = `<p class="codepen"
   data-class="${options.class}"
   data-height="${options.height}"
   data-theme-id="${options.theme}"
   data-default-tab="${options.tab}"
-  data-slug-hash="PowKVyN">
+  data-slug-hash="${slugHash}">
   <span><a href="${url}">See the Pen </a></span></p>`;
     expect(getCodepenHtml(options, url)).toBe(result);
   });
