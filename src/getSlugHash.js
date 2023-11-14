5081edcd29a9e1dfbd156ed54271419099c7c678
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2023-11-15 00:49:16 +0700

    fix regex

--- src/getSlugHash.js
+++ src/getSlugHash.js
@@ -6,7 +6,7 @@
  * @return {string}
  */
 const getSlugHash = (path) => {
-  const regex = /^\/(?<author>[\w-]{0,127})\/(?<pen>\w{3})\/(?<slug>\w{1,127})(?:\/(?<hash>\w{1,127})){0,1}.{0,127}$/u;
+  const regex =  new RegExp(`^\\/(?<author>[\\w-]{0,127})\\/(?<pen>\\w{3})\\/(?<slug>\\w{1,127})(?:\\/(?<hash>\\w{1,127})){0,1}\\w{0,127}$`, 'u');
   const matches = regex.exec(path);
 
   return matches.groups.hash ? matches.groups.hash : matches.groups.slug ;
