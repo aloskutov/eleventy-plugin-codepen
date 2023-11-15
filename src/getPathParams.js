19574a68adfbb452534c1262ebd86390739ffa18
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2023-11-15 14:48:22 +0700

    Use a regular expression instead of the 'RegExp'

--- src/getPathParams.js
+++ src/getPathParams.js
@@ -6,12 +6,12 @@
  * @return {string}
  */
 const getPathParams = (path) => {
-  const regex =  new RegExp('^\\/(?<user>[\\w-]{0,127})\\/(?<pen>pen)\\/(?<slug>\\w{1,127})(?:\\/(?<hash>\\w{1,127})){0,1}\\w{0,127}$', 'ui');
+  const regex = /^\/(?<user>[\w\u002d]{0,127})\/pen\/(?<slug>\w{1,127})(?<hash>\/\w{1,127}){0,1}$/ui;
   const matches = regex.exec(path);
 
   return {
     user: matches.groups.user ? matches.groups.user : null,
-    hash: matches.groups.hash ? `${matches.groups.slug}/${matches.groups.hash}` :  matches.groups.slug
+    hash: matches.groups.hash ? `${matches.groups.slug}${matches.groups.hash}` :  matches.groups.slug
   };
 };
 
